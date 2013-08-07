36418a94c8da6710796aadfb1e8ca2f689503a29
Philip McNamara <phil@tapmap.com>
2013-08-07 16:06:21 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootTest::Application.config.secret_key_base = '3aa66da0157caedbb8ad47319af4b2074f079b2346562ddc2466dfa94f6b820bad207de007f908386b03751589c49d0a5dc5cdf587dcae8e97c3323e5feaf6cb'
